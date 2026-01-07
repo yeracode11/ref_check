f05ed614ee30bb7d0ba182ebc139e13ee3558094
yeracode11 <ersul143@gmail.com>
2026-01-07 22:51:11 +0500

    nginx fixes

--- fridge-manager/routes/admin.js
+++ fridge-manager/routes/admin.js
@@ -12,7 +12,7 @@ const XLSX = require('xlsx');
 const upload = multer({ 
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 100 * 1024 * 1024, // 10MB максимум
+    fileSize: 100 * 1024 * 1024, // 100MB максимум
   },
   fileFilter: (req, file, cb) => {
     console.log('[Multer] File filter called:', {
